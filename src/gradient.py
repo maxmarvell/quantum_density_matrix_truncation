8c071cae67196e1dddb4bef377f73f22fd32aedc
maxmarvell <max.marvell@hotmail.co.uk>
2025-05-11 15:57:54 +0100

    removing garbage files

--- src/gradient.py
+++
@@ -1,8 +0,0 @@
-from ncon import ncon
-import numpy as np
-from uniform_MPS import *
-from transfer_matrix import *
-from fixed_point import *
-
-
-
--- src/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-    # f = EvolvedHilbertSchmidt(A, B, U, U, 6)
-    # Op = OpimisationProblem(f)
-    # Op.optimize(grassman_retraction, 1000, 1e-5)
