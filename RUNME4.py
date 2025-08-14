500c9da81c8bba1fec4cadccfcdfd47520518286
maxmarvell <max.marvell@hotmail.co.uk>
2025-08-14 17:27:37 +0100

    change: prepare for god run.

--- RUNME4.py
+++ RUNME4.py
@@ -10,20 +10,20 @@ from qdmt.evolve import load_state, check_write_permission, evolve
 
 def main():
 
-    loadfile = 'data/ground_state/gstate_ising2_D2_g1.5.npy'
+    loadfile = 'data/ground_state/gstate_ising2_D8_g1.5.npy'
     A, prev_data, start_time = load_state(loadfile)
 
-    filepath = 'data/non_integrable/bond_dimension_2_patch_4'
+    filepath = 'data/non_integrable/bond_dimension_14_patch_8'
     assert check_write_permission(filepath)
 
     model = TransverseFieldIsing(g=0.2, delta_t=0.1)
-    times, state, cost, norm = evolve(A, 2, 4, model, 0.05, 20, 100, 1e-8, start_time)
+    times, state, cost, norm = evolve(A, 14, 8, model, 0.1, 20, 100, 1e-8, start_time)
 
-    if prev_data:
-        times = np.concatenate((prev_data['time'], times))
-        state = np.concatenate((prev_data['state'], state))
-        cost = np.concatenate((prev_data['cost'], cost))
-        norm = np.concatenate((prev_data['gradient_norm'], norm))
+    # if prev_data:
+    #     times = np.concatenate((prev_data['time'], times))
+    #     state = np.concatenate((prev_data['state'], state))
+    #     cost = np.concatenate((prev_data['cost'], cost))
+    #     norm = np.concatenate((prev_data['gradient_norm'], norm))
 
     np.savez_compressed(filepath,
                         time=times,
